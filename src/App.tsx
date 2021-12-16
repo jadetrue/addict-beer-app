edb4f5e43ba7b298d558f5cdd7e2c8b5bba6e5da
Jade True <jadetruexo@gmail.com>
2021-12-16 16:13:04 +0000

    Updated SearchBar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./styles/typography.css";
 import "./styles/global.css";
 import CardList from "./components/CardList/CardList";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
     return (
         <div>
+            <SearchBar />
             <CardList />
         </div>
     );
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBar = () => {
+    return (
+        <div className="flex justify-center my-20">
+            <input className="rounded-lg p-2" placeholder="Find a beer" />
+        </div>
+    );
+};
+
+export default SearchBar;
